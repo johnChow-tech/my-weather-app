e7a8a75b2a64e02855a0591d458c4cb25b578a7f
johnChow-tech <shulking@hotmail.com>
2025-07-30 13:01:38 +0900

    feat: add elements getter to main.js

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
   // element collections
   const element = {
+    //top page
+    mainLogo: document.querySelector('.main-logo'),
+    inputBox: document.querySelector('.input-box'),
+    alertCityNotFound: document.querySelector('.alert-city-not-found'),
+    popularCities: document.querySelector('.popular-cities'),
+    // result page
+    weatherDesc: document.querySelector('.weather-desc'),
+    weatherIcon: document.querySelector('.weather-icon'),
     weatherInfoCards: document.querySelectorAll('.weather-info-card'),
   };
+
   // TODO: add functions
   clickWeatherInfoCard();
 
-  /**
-   *
-   */
   function clickWeatherInfoCard() {
     element.weatherInfoCards.forEach((card) => {
       card.addEventListener('click', () => {
@@ -16,4 +22,6 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     });
   }
+
+  function clickPopularCitiesLabel() {}
 });
