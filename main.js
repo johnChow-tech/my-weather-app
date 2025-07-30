06da99f829094e1756d7b0d2af75cc307461f21a
johnChow-tech <shulking@hotmail.com>
2025-07-30 21:52:01 +0900

    feat: add weather API fetch function - add API_KEY to config.js and - add reference to config.js in index.html

--- main.js
+++ main.js
@@ -28,7 +28,26 @@ function clickWeatherInfoCard() {
 function clickPopularCitiesLabel() {
   elements.popularCitiesChips.forEach((chip) => {
     chip.addEventListener('click', () => {
-      console.log(chip.label);
+      elements.searchTextField.value = chip.label;
+      fetchWeatherData(chip.label);
     });
   });
+}
+
+elements.searchTextField.addEventListener('keydown', (e) => {
+  if (e.code === 'Enter') {
+    fetchWeatherData(elements.searchTextField.value);
+  }
+});
+
+async function fetchWeatherData(cityName) {
+  const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${cityName}&aqi=no`;
+  try {
+    const request = new Request(apiUrl);
+    const response = await fetch(request);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('Failed to fetch weather data:', error);
+  }
 }
