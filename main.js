d1ff63fba38cac7b5c57206097fd0be2751524c0
johnChow-tech <shulking@hotmail.com>
2025-07-30 13:04:40 +0900

    feat: change script loadign approach. - Moved script tag to head - added defer script tag

--- main.js
+++ main.js
@@ -1,27 +1,25 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // element collections
-  const element = {
-    //top page
-    mainLogo: document.querySelector('.main-logo'),
-    inputBox: document.querySelector('.input-box'),
-    alertCityNotFound: document.querySelector('.alert-city-not-found'),
-    popularCities: document.querySelector('.popular-cities'),
-    // result page
-    weatherDesc: document.querySelector('.weather-desc'),
-    weatherIcon: document.querySelector('.weather-icon'),
-    weatherInfoCards: document.querySelectorAll('.weather-info-card'),
-  };
+// element collections
+const element = {
+  //top page
+  mainLogo: document.querySelector('.main-logo'),
+  inputBox: document.querySelector('.input-box'),
+  alertCityNotFound: document.querySelector('.alert-city-not-found'),
+  popularCities: document.querySelector('.popular-cities'),
+  // result page
+  weatherDesc: document.querySelector('.weather-desc'),
+  weatherIcon: document.querySelector('.weather-icon'),
+  weatherInfoCards: document.querySelectorAll('.weather-info-card'),
+};
 
-  // TODO: add functions
-  clickWeatherInfoCard();
+// TODO: add functions
+clickWeatherInfoCard();
 
-  function clickWeatherInfoCard() {
-    element.weatherInfoCards.forEach((card) => {
-      card.addEventListener('click', () => {
-        console.log('卡片被点击了');
-      });
+function clickWeatherInfoCard() {
+  element.weatherInfoCards.forEach((card) => {
+    card.addEventListener('click', () => {
+      console.log('卡片被点击了');
     });
-  }
+  });
+}
 
-  function clickPopularCitiesLabel() {}
-});
+function clickPopularCitiesLabel() {}
